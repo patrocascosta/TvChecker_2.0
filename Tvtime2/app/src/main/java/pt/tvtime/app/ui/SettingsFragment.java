a1eaf3c170c6a3022eb7d26654466188f4e1be0b
godofanimes <46845347+godofanimes@users.noreply.github.com>
2020-01-19 13:30:02 +0000

    Settings

--- Tvtime2/app/src/main/java/pt/tvtime/app/ui/SettingsFragment.java
+++ Tvtime2/app/src/main/java/pt/tvtime/app/ui/SettingsFragment.java
@@ -9,9 +9,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
 import pt.tvtime.app.R;
+import pt.tvtime.app.adapters.FavoritosAdapter;
 
 public class SettingsFragment extends Fragment {
+
+    private ListView listView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,16 @@ public class SettingsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
+
+        listView = (ListView)findViewById(R.id.listViewSettings);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("Logout");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.settingsrow, arrayList);
+
+        listView.setAdapter(arrayAdapter);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
