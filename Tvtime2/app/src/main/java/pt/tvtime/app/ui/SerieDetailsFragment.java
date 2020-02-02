25223be6e473a00f7287c28011c345dd2563ebab
godofanimes <46845347+godofanimes@users.noreply.github.com>
2020-02-02 21:28:30 +0000

    update

--- Tvtime2/app/src/main/java/pt/tvtime/app/ui/SerieDetailsFragment.java
+++ Tvtime2/app/src/main/java/pt/tvtime/app/ui/SerieDetailsFragment.java
@@ -1,15 +1,24 @@
 package pt.tvtime.app.ui;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import pt.tvtime.app.R;
+import pt.tvtime.app.model.Serie;
+import pt.tvtime.app.repository.local.AppDatabase;
 
 
 /**
@@ -17,17 +26,37 @@ import pt.tvtime.app.R;
  */
 public class SerieDetailsFragment extends Fragment {
 
+    private ImageView imageViewSerie;
+    private TextView textViewPontuacao;
+    private TextView textViewName;
+    private TextView textViewDescricao;
+    Context context;
 
     public SerieDetailsFragment() {
         // Required empty public constructor
     }
 
-
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_serie_details, container, false);
+        View view = inflater.inflate(R.layout.fragment_serie_details, container, false);
+
+        long serieId = getArguments().getLong("serieId");
+
+        Serie serie = AppDatabase.getInstance(getContext()).getSeriesDAO().getSerieById(serieId);
+
+        this.imageViewSerie = view.findViewById(R.id.imageViewHome);
+        this.textViewDescricao = view.findViewById(R.id.textViewDescricao);
+        this.textViewName = view.findViewById(R.id.textViewName);
+        this.textViewPontuacao = view.findViewById(R.id.textViewPontuacao);
+
+        Glide.with(this.context).load(serie.getImagem()).into(imageViewSerie);
+        textViewPontuacao.setText(serie.getImdb());
+        textViewName.setText(serie.getNomeSerie());
+        textViewDescricao.setText(serie.getDescricao());
+
+        return view;
     }
 
 }
