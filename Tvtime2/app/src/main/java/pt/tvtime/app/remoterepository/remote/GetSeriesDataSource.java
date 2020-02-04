515ec2000ddd3bebf0a60cb1b4073e5454676790
godofanimes <46845347+godofanimes@users.noreply.github.com>
2020-02-04 11:56:00 +0000

    update

--- Tvtime2/app/src/main/java/pt/tvtime/app/remoterepository/remote/GetSeriesDataSource.java
+++ Tvtime2/app/src/main/java/pt/tvtime/app/remoterepository/remote/GetSeriesDataSource.java
@@ -10,5 +10,10 @@ public class GetSeriesDataSource {
             .addConverterFactory(GsonConverterFactory.create())
             .build().create(GetSeriesService.class);
 
+   /* private static GetSeriesService service = new Retrofit.Builder()
+            .baseUrl("http://localhost:8000/api/serie/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build().create(GetSeriesService.class);*/
+
     public static GetSeriesService getService() {return service;}
 }
